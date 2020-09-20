308bd95bc4e82457e9756bcf22ef55ae163fe96d
Ayush Dewan <ayushdewan02@gmail.com>
2020-09-19 22:23:37 -0400

    pwr code

--- application.py
+++ application.py
@@ -3,10 +3,10 @@ app = Flask(__name__)
 
 import json
 import os
+import subprocess
 
 import flask
 from time import sleep
-
 
 app = flask.Flask(__name__)
 app.secret_key = "secret"
@@ -19,6 +19,10 @@ def index():
 def upload():
     return render_template("upload.html")
 
-@app.route('/user/<username>')
-def show_dash(username):
-    return render_template("dashboard.html", username=username)
+
+@app.route('/powerstat')
+def powerstat():
+    output = subprocess.Popen("sudo tlp-stat | grep -P '\[m(W|A)\]'", shell=True, stdout=subprocess.PIPE).stdout.read().decode("UTF-8")
+    output = int(output[output.find("=") + 1: output.find("[")].strip())
+    return output
+
